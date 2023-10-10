0df8d1c70253d75d7c6d5939603153bcd77b244b
Jacob <jacobboyd95@gmail.com>
2023-10-10 13:00:30 -0400

    Added exchange channel/partner handler class

--- python/interpolations.py
+++ python/interpolations.py
@@ -7,6 +7,7 @@ from scipy.interpolate import CubicSpline
 
 
 
+# 1D interpolation
 class interpol1D:
     def __init__(self,conf,tag):
         self.conf = conf
@@ -23,14 +24,24 @@ class interpol1D:
                 data_list.append(float(row[0]))
         data_array = np.array(data_list)
         return CubicSpline(np.linspace(float(header[0]),float(header[1]),len(data_array)),data_array)
-        
-        
-                
-                
-            
-            
-            
-        
-        
 
+# probability block gets either (a size or a list of blocks) AND a tag to know who it belonged to, 
+#potentially you could store the function objects for the results gere instead 
+class probBlock:
+    def __init__(self, inp, tag):
+        self.content=inp
+        self.tag = tag
+        if type(inp)==float:
+            self.size=inp
+        elif type(inp)==list:
+            self.size=sum([pB.size for pB in inp])
+    def __call__(self, r):
+        if type(self.content)==float:
+            return [self.tag]
+        runtot=0
+        for pB in self.content:
+            runtot+=pB.size
+            print(r-runtot)
+            if r < runtot:
+                return pB(r-runtot)+[self.tag]   
 
